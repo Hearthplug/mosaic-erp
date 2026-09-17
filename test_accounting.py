06f39d7c9484059eeb4ef4ae7038914674412951
Hearthplug <zia323798@gmail.com>
2026-09-17 14:26:34 -0500

    Test extended accounting controls

--- test_accounting.py
+++ test_accounting.py
@@ -34,3 +34,19 @@ class AccountingCase(unittest.TestCase):
   gl=self.a.general_ledger(self.w,self.account('cash')); self.assertEqual(gl['ending_balance_minor'],10000); self.assertIn('balance_sheet',self.a.financial_statements(self.w,to_date='2026-12-31'))
 
 if __name__=='__main__': unittest.main()
+
+class ExtendedAccountingCase(AccountingCase):
+ def test_opening_balances_payment_aging_and_fx(self):
+  self.a.opening_balance(self.w,'owner','2026-01-01',[{'account_id':self.account('cash'),'balance_minor':5000}],approved_by='accountant')
+  customer=self.a.create_party(self.w,'owner','customer','Buyer'); d=self.a.create_document(self.w,'owner','sales_invoice','2026-01-05',[{'description':'Sale','unit_price_minor':1000}],party_id=customer['id'],due_date='2026-01-10',currency='EUR',exchange_rate='1.1'); self.a.approve_document(self.w,'manager',d['id']); self.a.post_document(self.w,'owner',d['id'])
+  pay=self.a.record_payment(self.w,'owner',d['id'],400,'2026-02-15',exchange_rate='1.2'); self.assertEqual(pay['remaining_minor'],600); self.assertEqual(pay['realized_fx_minor'],40)
+  self.assertEqual(self.a.aging(self.w,'2026-02-15')['buckets']['31_60'],600)
+ def test_inventory_bank_migration_and_acceptance_controls(self):
+  item='itm_x'; self.s._db.execute("INSERT INTO items(id,workspace_id,sku,name) VALUES(?,?,?,?)",(item,self.w,'A','A'))
+  self.a.record_inventory_movement(self.w,'owner',item,'2026-01-01','3',250,'receipt'); self.assertEqual(self.a.inventory_valuation(self.w)['total_value_minor'],750)
+  bank=self.account('bank'); tx=self.a.import_bank_transactions(self.w,'owner',bank,[{'posted_on':'2026-01-02','description':'Deposit','amount_minor':100,'external_id':'x'}])[0]
+  j=self.a.post_journal(self.w,'owner','2026-01-02','Deposit',[{'account_id':bank,'debit_minor':100},{'account_id':self.account('equity'),'credit_minor':100}]); self.assertTrue(self.a.match_bank_transaction(self.w,'owner',tx['id'],j['id'])['matched'])
+  self.assertEqual(self.a.migration_reconciliation(self.w,'owner','legacy',2,100,90)['status'],'variance')
+  expected={'trial_balance':self.a.trial_balance(self.w)}; self.assertEqual(self.a.run_acceptance(self.w,'accountant','sample',expected)['status'],'passed')
+ def test_statutory_adapter_defaults_closed(self):
+  x=self.a.configure_statutory_adapter(self.w,'owner','IN','e-invoice','2026-01'); self.assertEqual(x['status'],'disabled')
